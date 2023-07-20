9e9f717cc1a4cec42a4e555b0e53e57d732d91c3
olga-alekseeva <oliasca@gmail.com>
2023-07-20 14:35:11 +0300

    Создала префабы Меню и кнопок

---
+++ Assets/_Root/Scripts/MenuStarter/MenuStarter.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal sealed class MenuStarter : MonoBehaviour
+{
+    private 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
